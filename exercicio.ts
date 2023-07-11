e5583913b39fce41ad70ad172a9e6541ec5c702e
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2023-07-11 00:18:32 -0300

    Initial commit

---
+++ exercicio.ts
@@ -0,0 +1,11 @@
+const multiply = (factor1: number, factor2: number): number => factor1 * factor2;
+
+const result = multiply(2, 2)
+console.log(result);
+
+
+
+const sayHello = (name: string): string => `Olá ${name}!`;
+
+const welcome = sayHello('Marcelo');
+console.log(welcome);
